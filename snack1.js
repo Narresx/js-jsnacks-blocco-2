c88e25ddd7cb681797ea8e9513a491cfcf84a5ea
Narresx <92748159+Narresx@users.noreply.github.com>
2022-01-13 14:56:31 +0100

    add first snack

--- snack1.js
+++ snack1.js
@@ -6,3 +6,14 @@ Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while.
 */
 
+let sum = 0; // Poniamo una variabile fuori dal ciclo dove andremo a sommare i 5 numeri dell'utente.
+// Iniziamo il ciclo 
+let = i = 0; // Flag che serve per far continuare a contare il programma
+
+while (i < 5) { // Finchè i non arriva a fare 5 giri
+    const number = parseInt(prompt('Inserisci un numero.')); // Chiediamo i numeri all'utente
+    sum += number; // Aggiungiamo alla somma i numeri che ci da l'utente
+    i++;
+    console.log(sum);
+}
+console.log(sum)
