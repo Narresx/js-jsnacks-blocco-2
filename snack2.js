be11470d29419ade1dc3b16aea3ff7b2ffc22243
Narresx <92748159+Narresx@users.noreply.github.com>
2022-01-13 15:46:40 +0100

    add second snack adjustment

--- snack2.js
+++ snack2.js
@@ -5,20 +5,16 @@ Inserisci un numero, se è pari stampa il numero,
 se è dispari stampa il numero successivo
 */
 
-let number = Math.floor(Math.random() * 100); // Sciviamo un numero casuale
+let number = parseInt(prompt('Inserisci un numero.')); // Sciviamo un numero casuale
 console.log(number);
 
 let even = 2;
-/*
+
 if (number % even === 0) {
     console.log(number);
 } else {
-    number = Math.floor(Math.random() * 100);
+    console.log(number + 1)
 }
-*/
+
 //Il secondo numero in console è pari, altrimenti vedo solo un numero
 
-while (number % even === 0) {
-    console.log(number);
-    break;
-}
